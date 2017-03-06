bcf7c4b1ba5a20a5fe894b5c4858618667196632
Yemi Bedu @ P&R <yemi@weldfast.com>
2017-03-06 12:36:51 -0500

    Update extension.ts to support new color modes

--- src/extension.ts
+++ src/extension.ts
@@ -8,21 +8,41 @@ interface Highlight {
 	tooltip: string;
 }
 
+interface Highlight2 {
+	filename: string;
+	text: string;
+	bcolor: string;
+	fcolor: string;
+	dcolor: string;
+	tooltip: string;
+}
+
 interface DecorationArray {
 	[color: string] : vscode.TextEditorDecorationType
 }
 
 let decorationTypes: DecorationArray;
 let highlights: Highlight[];
+let highlights2: Highlight2[];
 let updateTimeout: number;
 
 function loadConfig() : void {
-	console.log('badCodeHighlighter loading config');
-	let config = vscode.workspace.getConfiguration('badCodeHighlighter');
+	console.log('badCodeHighlighter2 loading config');
+	let config = vscode.workspace.getConfiguration('badCodeHighlighter2');
 	updateTimeout = config.get<number>('updateTimeout', 250);
 	highlights = config.get<Highlight[]>('highlights', []);
 	if(highlights && highlights.length > 0) {
 		let allDecoratorStyles : string[] = _.uniq(highlights.map(h => h.color));
+		decorationTypes = allDecoratorStyles.reduce<DecorationArray>((accum, val) => {
+			accum[val] = vscode.window.createTextEditorDecorationType({
+				backgroundColor: val,
+			});
+			return accum;
+		},{});
+	}
+	highlights2 = config.get<Highlight2[]>('highlights', []);
+	if(highlights2 && highlights2.length > 0) {
+		let allDecoratorStyles : string[] = _.uniq(highlights2.map(h => h.bcolor));
 		decorationTypes = allDecoratorStyles.reduce<DecorationArray>((accum, val) => {
 			accum[val] = vscode.window.createTextEditorDecorationType({
 				backgroundColor: val,
@@ -66,13 +86,21 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!activeEditor) {
 			return;
 		}
-
+		
 		// maybe move this check into foreach
 		let activeHighlights = highlights.filter(h => activeEditor.document.fileName.match(h.filename));
-		if(activeHighlights.length == 0) {
-			return;
+		if(activeHighlights.length > 0) {
+			updateDecoration(activeHighlights);
 		}
-
+		
+		// maybe move this check into foreach
+		let activeHighlight2 = highlights2.filter(h => activeEditor.document.fileName.match(h.filename));
+		if(activeHighlights2.length > 0) {
+			updateDecoration(activeHighlights2);
+		}
+	}
+	
+	function updateDecoration(activeHighlights){
 		const text = activeEditor.document.getText();
 
 		let activeDecorationsByColor: { [color: string]: vscode.DecorationOptions[] } = {};
